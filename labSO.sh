ddeca8f5bd361d016574f127877139528770be71
Andre Leon S. Gradvohl <gradvohl@ft.unicamp.br>
2019-04-02 09:55:21 -0300

    Correcting script and codes

--- labSO.sh
+++ labSO.sh
@@ -1,59 +1,54 @@
 #!/bin/bash
 #
-# Programa desenvolvido por:
-#     Andre Leon S. Gradvohl, Dr.
+# Programa desenvolvido por Prof. Andre Leon S. Gradvohl, Dr.
 # para preparar o laboratorio de sistemas operacionais.
-# Ultima atualizacao: 02/11/2011
+#
+# Ultima versao: seg 02 abr 2019 13:51:22 -03
 
-echo -e "Esse programa vai preparar o ambiente para o\n laboratorio de sistemas operacionais"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHTBLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+echo -e "Esse script vai preparar o ambiente para o ${LIGHTBLUE}laboratorio de sistemas operacionais${NC}"
 
 #Primeiro Passo: Definicao de variaveis de ambiente
-
+# Arquivo compactado
 ARQCOMPACT=labSO.tar.gz
+#
+# Programa tar
 TAR=`which tar`
 if [ $? -ne 0 ]; then
-	echo "Programa para descompactacao nao foi encontrado!"
-	echo "Abortando Script"
-    exit 1;
+   echo "Programa para descompactacao ${RED}nao foi encontrado!${NC}"
+   echo -e "\nAbortando script... ${RED}Contacte o professor!${NC}"
+   exit 1;
 fi
-
-BROWSER=`which firefox`
-if [ $? -ne 0 ]; then
-    echo -e "Browser nao encontrado!\nTestando 2a. opcao"
-	BROWSER=`which mozilla`
-	if [ $? -ne 0 ]; then
-		    echo  "Browser nao encontrado!"
-		    echo "Abortando Script"
-			exit 1;
-	fi		
-fi
-
-ARQHTML=$PWD/laboratorio/index.html
+#
+# Diretorio de destino
+DESTDIR=${HOME}/laboratorioSO
 
 #Segundo Passo: descompactar arquivo de instrucoes
 if [ ! -f $ARQCOMPACT ]; then 
-    echo -e "Arquivo ${ARQCOMPACT} nao existe! \n Abortando..."
-	echo "Contacte o professor!"  
-    exit 1
+   echo -e "Arquivo ${ARQCOMPACT} ${RED} nao existe!${NC}"
+   echo -e "\nAbortando script... ${RED}Contacte o professor!${NC}"  
+   exit 1
 else
-    ${TAR} xzf ${ARQCOMPACT}
-    if [ $? -eq 0 ]; then 
-        echo "Arquivo ${ARQCOMPACT} descompactado com sucesso!"
-    else
-        echo "Arquivo ${ARQCOMPACT} com problemas na descompactacao"
-        echo "Abortando... Contacte o professor!"
+    mkdir -p ${DESTDIR}
+    if [ $? -ne 0 ]; then 
+        echo -e "Problemas na criação do diretorio ${DESTDIR} !"
+        echo -e "\nAbortando script... ${RED}Contacte o professor!${NC}"
         exit 1
     fi
-fi
 
-echo "Disparando browser: $BROWSER file://${ARQHTML}"
-$BROWSER file://${ARQHTML} &
-
-if [ $? -ne 0 ]; then
-    echo "Nao foi possivel instanciar o browser ${BROWSER}!"
-    echo "Contacte o professor! Abortando!"
-    exit 1
-else
-    echo "Ambiente preparado! Bom proveito!"
-    exit 0
+    ${TAR} xzf ${ARQCOMPACT} -C ${DESTDIR}
+    if [ $? -eq 0 ]; then 
+        echo -e "Arquivo ${ARQCOMPACT} descompactado ${GREEN} com sucesso!${NC}"
+        echo -e "Mude para o diretorio ${RED}${DESTDIR}${NC} e aguarde novas instrucoes. Use o comando a seguir:"
+        echo -e "  ${LIGHTBLUE}cd ${RED}${DESTDIR}${NC}"
+        exit 0
+    else
+        echo -e "Arquivo ${ARQCOMPACT} com ${RED}problemas na descompactacao${NC}"
+        echo -e "\nAbortando script... ${RED}Contacte o professor!${NC}"
+        exit 1
+    fi
 fi
